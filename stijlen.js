2a5187dfc9a584c26c7e3a3410d12b1b813804ff
Tariq Dilrosun <42801356+TariqD@users.noreply.github.com>
2018-11-08 11:24:56 +0100

    N

---
+++ stijlen.js
@@ -0,0 +1,19 @@
+let divje=document.getElementById("bl1");
+divje.style.backgroundColor ="deepskyblue";
+divje.style.paddingLeft ="20px";
+divje.style.paddingTop ="15px";
+
+
+let div=document.getElementById("bl2");
+div.style.backgroundColor ="green";
+div.style.marginTop = "14px";
+div.style.paddingLeft= "20px";
+
+
+let di=document.getElementById("bl3");
+di.style.backgroundColor ="yellow";
+di.style.marginTop = "38px";
+di.style.paddingLeft = "45px";
+
+let d=document.getElementById("bl4");
+d.style.backgroundColor ="red";
